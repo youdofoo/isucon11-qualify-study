0f7ebf3a34030f9d81d589adf6015ee525eea763
FUKUNAGA Shunya <fukushun1994@gmail.com>
2022-05-27 01:25:29 +0900

    Revert "add character index to isu"
    This reverts commit 55f5154e8dac255d1363da1bf95c665fdfd12010.

--- sql/0_Schema.sql
+++ sql/0_Schema.sql
@@ -12,8 +12,7 @@ CREATE TABLE `isu` (
   `jia_user_id` VARCHAR(255) NOT NULL,
   `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
   `updated_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
-   PRIMARY KEY(`id`),
-   INDEX idx_character (`character`)
+   PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
 
 CREATE TABLE `isu_condition` (
