f1aa8d89dbab79b252de52e5e7000fe76b820fb8
FUKUNAGA Shunya <fukushun1994@gmail.com>
2022-05-27 21:05:59 +0900

    fix patch sql

--- sql/2_Patch.sql
+++ sql/2_Patch.sql
@@ -2,4 +2,4 @@ ALTER TABLE isu_condition ADD COLUMN `level` VARCHAR(16) DEFAULT "warning";
 UPDATE isu_condition SET `level` = "info" WHERE `condition` = "is_dirty=false,is_overweight=false,is_broken=false";
 UPDATE isu_condition SET `level` = "critical" WHERE `condition` = "is_dirty=true,is_overweight=true,is_broken=true";
 
-ALTER TABLE isu DELETE COLUMN `image`;
+ALTER TABLE isu DROP COLUMN `image`;
