700a5c66e102f27801061a9857b92d354960f9f9
FUKUNAGA Shunya <fukushun1994@gmail.com>
2022-05-27 19:58:27 +0900

    add image extractor

---
+++ image-extractor/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type Isu struct {
+	ID         int       `db:"id" json:"id"`
+	JIAIsuUUID string    `db:"jia_isu_uuid" json:"jia_isu_uuid"`
+	Name       string    `db:"name" json:"name"`
+	Image      []byte    `db:"image" json:"-"`
+	Character  string    `db:"character" json:"character"`
+	JIAUserID  string    `db:"jia_user_id" json:"-"`
+	CreatedAt  time.Time `db:"created_at" json:"-"`
+	UpdatedAt  time.Time `db:"updated_at" json:"-"`
+}
+
+type MySQLConnectionEnv struct {
+	Host     string
+	Port     string
+	User     string
+	DBName   string
+	Password string
+}
+
+func getEnv(key string, defaultValue string) string {
+	val := os.Getenv(key)
+	if val != "" {
+		return val
+	}
+	return defaultValue
+}
+
+func NewMySQLConnectionEnv() *MySQLConnectionEnv {
+	return &MySQLConnectionEnv{
+		Host:     getEnv("MYSQL_HOST", "127.0.0.1"),
+		Port:     getEnv("MYSQL_PORT", "3306"),
+		User:     getEnv("MYSQL_USER", "isucon"),
+		DBName:   getEnv("MYSQL_DBNAME", "isucondition"),
+		Password: getEnv("MYSQL_PASS", "isucon"),
+	}
+}
+
+func (mc *MySQLConnectionEnv) ConnectDB() (*sqlx.DB, error) {
+	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?interpolateParams=true&parseTime=true&loc=Asia%%2FTokyo", mc.User, mc.Password, mc.Host, mc.Port, mc.DBName)
+	return sqlx.Open("mysql", dsn)
+}
+
+func main() {
+	mySQLConnectionData := NewMySQLConnectionEnv()
+	db, err := mySQLConnectionData.ConnectDB()
+	if err != nil {
+		log.Fatalf("failed to connect db: %v", err)
+		return
+	}
+	db.SetMaxOpenConns(10)
+	defer db.Close()
+
+	isuList := make([]Isu, 0)
+
+	err = db.Select(&isuList, "SELECT * FROM `isu`")
+	if err != nil {
+		log.Fatalf("failed to select isu: %v", err)
+	}
+
+	for _, isu := range isuList {
+		file, err := os.Create(isu.JIAIsuUUID)
+		if err != nil {
+			log.Fatalf("failed to create file: %v", err)
+		}
+		_, err = file.Write(isu.Image)
+		if err != nil {
+			file.Close()
+			log.Fatalf("failed to write file: %v", err)
+		}
+		file.Close()
+	}
+}
